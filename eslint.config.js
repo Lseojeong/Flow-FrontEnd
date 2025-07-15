f1b1cc1715ca4b7dcbc8bfe1c603a5c4c34929b0
Lseojeong <sjung0314@gachon.ac.kr>
2025-07-16 02:30:14 +0900

    Chore : ESLint와 Prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    ignores: ['eslint.config.js', 'dist/**', 'node_modules/**'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
     },
+    plugins: {
+      js,
+      react: pluginReact,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
-])
+  // prettier 연동
+  {
+    rules: {
+      ...prettier.rules,
+    },
+  },
+]);
