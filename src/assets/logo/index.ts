d7603e04171553a91e46c9f2de09e50b4db60fef
Lseojeong <sjung0314@gachon.ac.kr>
2025-07-18 22:21:37 +0900

    Chore : png → webp 이미지 포맷 교체

---
+++ src/assets/logo/index.ts
@@ -0,0 +1,4 @@
+export { default as symbolLogo } from './symbol.webp';
+export { default as symbolTextLogo } from './symbolText.webp';
+export { default as textV1Logo } from './textV1.webp';
+export { default as textV2Logo } from './textV2.webp';
