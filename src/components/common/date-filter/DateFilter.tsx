694ce6ebe315a229c1e20ed3676da06fafd44039
Lseojeong <sjung0314@gachon.ac.kr>
2025-07-25 11:21:42 +0900

    Refactor : 절대 경로로 변경

--- src/components/common/date-filter/DateFilter.tsx
+++ src/components/common/date-filter/DateFilter.tsx
@@ -23,7 +23,7 @@ import {
   INPUT_RADIUS,
   INPUT_PADDING,
   NAVIGATION_BUTTON_SIZE,
-} from '../input/input.constants';
+} from '@/components/common/input/input.constants';
 
 registerLocale('ko', ko);
 
