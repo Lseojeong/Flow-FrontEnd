188427c35dc2b704d72a2a785a6bae3ee1ae4597
Lseojeong <sjung0314@gachon.ac.kr>
2025-07-27 22:15:51 +0900

    Refactor : 폴더 구조 통일

--- src/components/common/table/index.ts
+++
@@ -1,4 +0,0 @@
-export { TableLayout } from './TableLayout';
-export { TableHeader } from './TableHeader';
-export { TableRow, TableCell } from './TableRow';
-export { ScrollableCell } from './ScrollableCell';
