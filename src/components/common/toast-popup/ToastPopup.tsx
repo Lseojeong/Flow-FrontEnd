2c252e615b12a0d39fbf3d752d13b26d82baa1b6
Lseojeong <sjung0314@gachon.ac.kr>
2025-08-03 01:59:11 +0900

    Design : 아이콘 추가

--- src/components/common/toast-popup/ToastPopup.tsx
+++ src/components/common/toast-popup/ToastPopup.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { colors, fontWeight } from '@/styles';
 import { ToastProps } from './ToastPopup.types';
+import { CompletedIcon } from '@/assets/icons/common/index';
 
 export const Toast: React.FC<ToastProps> = ({ message, duration = 5000, onClose }) => {
   useEffect(() => {
@@ -12,7 +13,12 @@ export const Toast: React.FC<ToastProps> = ({ message, duration = 5000, onClose 
     return () => clearTimeout(timer);
   }, [duration, onClose]);
 
-  return <ToastContainer>{message}</ToastContainer>;
+  return (
+    <ToastContainer>
+      <CompletedIcon />
+      {message}
+    </ToastContainer>
+  );
 };
 
 const fadeInOut = keyframes`
@@ -46,4 +52,13 @@ const ToastContainer = styled.div`
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
   animation: ${fadeInOut} 5s ease forwards;
   pointer-events: auto;
+  display: flex;
+  align-items: center;
+  gap: 4px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+  }
 `;
