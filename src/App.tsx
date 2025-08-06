16d35aea57e6fe2542c00fd3efbc8677856e968f
Lseojeong <sjung0314@gachon.ac.kr>
2025-08-06 13:08:53 +0900

    Feat: React Query 및 Axios 인스턴스 추가, 환경 변수 설정 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import routes from '@/routes/router';
 import GlobalStyle from '@/styles/globalStyle';
 import PlaygroundRoutes from '@/routes/playgroundRoutes';
 import { ToastContainer } from '@/components/common/toast-popup/ToastContainer';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@/reactQuery';
 
 const App: React.FC = () => {
   const element = useRoutes(routes);
 
   return (
     <>
-      <GlobalStyle />
-      <ToastContainer />
-      {element}
-      {/* 개발 환경에서만 플레이그라운드 라우트 추가 */}
-      {import.meta.env.DEV && <PlaygroundRoutes />}
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <ToastContainer />
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={true} />}
+        {element}
+        {/* 개발 환경에서만 플레이그라운드 라우트 추가 */}
+        {import.meta.env.DEV && <PlaygroundRoutes />}
+      </QueryClientProvider>
     </>
   );
 };
